550ab50cfc5e25897e9121e302fff957ff53d5cc
Silver Valdvee <silver_valdvee@hotmail.com>
2022-07-23 01:54:25 +0300

    Add ability to hide errors

--- kucherx/windows/errors.py
+++ kucherx/windows/errors.py
@@ -10,6 +10,16 @@ def make_errors_window(dpg, state: KucherXState):
     with dpg.window(label="Configure interface", width=700, height=400, no_close=False) as current_window_id:
         counter = 0
 
+        def hide_errors():
+            """While clearing the items is only possible by making a new window, it is possible to hide the items"""
+            for i in range(counter):
+                try:
+                    dpg.hide_item(f"error{i}")
+                except Exception as e:
+                    logger.error(e)
+
+        dpg.add_button(label="Hide errors", callback=hide_errors)
+
         def add_error_to_display(error: str):
             nonlocal counter
             dpg.add_text(default_value=error, tag=f"error{counter}", parent=current_window_id, show=True)
