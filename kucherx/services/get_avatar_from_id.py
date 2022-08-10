1a4f9cfce021a17a4ed02211611f7a540a9d908f
Silver Valdvee <silver_valdvee@hotmail.com>
2022-08-10 12:37:18 +0300

    Clean some old files.
    These were left over, migrating from dearpygui

--- kucherx/services/get_avatar_from_id.py
+++
@@ -1,7 +0,0 @@
-from kucherx.domain.avatar import Avatar
-from kucherx.domain import HWID
-from kucherx.domain.god_state import GodState
-
-
-def get_avatar_from_id(state: GodState, _id: HWID) -> Avatar:
-    return state.avatar.avatars_by_hw_id.get(_id)
