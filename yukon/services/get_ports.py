e4bb7a205f2783b2adade335dd918ff2aa0e9c3a
Silver Valdvee <silver_valdvee@hotmail.com>
2022-08-19 13:34:58 +0300

    Fix CI

--- yukon/services/get_ports.py
+++ yukon/services/get_ports.py
@@ -1,9 +1,10 @@
+import typing
 from serial.tools.list_ports_common import ListPortInfo
 from serial.tools import list_ports
 from can.interfaces.socketcan import utils as socketcan_utils
 
 
-def ListPortInfo_to_dict(list_port_info: ListPortInfo):
+def ListPortInfo_to_dict(list_port_info: ListPortInfo) -> typing.Any:
     return {
         'device': list_port_info.device,
         'description': list_port_info.description,
@@ -11,9 +12,9 @@ def ListPortInfo_to_dict(list_port_info: ListPortInfo):
     }
 
 
-def get_slcan_ports():
+def get_slcan_ports() -> typing.List[typing.Any]:
     return list(map(ListPortInfo_to_dict, list_ports.comports()))
 
 
-def get_socketcan_ports():
+def get_socketcan_ports() -> typing.List[str]:
     return list(socketcan_utils.find_available_interfaces())
