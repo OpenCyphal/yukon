17808d657b8eeaf3f0c823aa28428cbacc9f1184
Silver Valdvee <silver_valdvee@hotmail.com>
2022-10-03 19:08:06 +0300

    Fix #124

--- yukon/html/monitor/context-menu.module.js
+++ yukon/html/monitor/context-menu.module.js
@@ -176,7 +176,6 @@ export function make_context_menus(yukon_state) {
                     copyTextToClipboard(register_value);
                 }
             },
-            shouldBeDisplayed: oneSelectedConstraint
         },
         {
             content: `${copyIcon}Copy selected as yaml`,
