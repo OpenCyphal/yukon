820f00b4b1cbbc689bd39d70e906e55cd5bc7bc6
Silver Valdvee <silver.valdvee@zubax.com>
2022-12-09 20:55:11 +0200

    Progress

--- yukon/services/cyphal_worker/forward_dronecan_work.py
+++ yukon/services/cyphal_worker/forward_dronecan_work.py
@@ -10,7 +10,7 @@ from yukon.domain.god_state import GodState
 
 logger = logging.getLogger(__name__)
 
-logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.DEBUG)
 
 def make_handler_for_transmit(state: GodState):
     def handle_transmit_message_to_dronecan(can_capture: pycyphal.transport.can.CANCapture) -> None:
